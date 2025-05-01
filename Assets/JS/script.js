d684233b87f2b219f8eec350706b82d5503d9af5
Tamal Pakrasi <tamalpakrasi11@gmail.com>
2025-05-02 00:36:07 +0530

    store event data in variables

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -50,14 +50,64 @@ $(document).ready(function () {
           return [true, 'ui-state-highlight', 'Today'];
         }
         return [true, ''];
+      },
+      onSelect: function (dateText, inst) {
+        const date = dateText;
+        $('#floatingDate').val(dateText);
       }
     });
-  });
+    // $('.ui-datepicker-calendar').find('a').attr('data-bs-toggle', 'modal');
+    // $('.ui-datepicker-calendar').find('a').attr('data-bs-target', "#exampleModalCenteredScrollable");
+  }
+  );
 
   $('#myTable').DataTable({
     pageLength: 3,
 
     // optional: change the dropdown choices
     lengthMenu: [3, 6, 9, 12],
+  });
+
+  function formatDate(modalBody) {
+    const dateArray = modalBody.find('#floatingDate').val().split('-');
+    if (dateArray) {
+      const [year, month, day] = dateArray;
+      return `${day}-${month}-${year}`;
+    }
+  }
+
+  function formatTime(modalBody) {
+    const timeArray = modalBody.find('#floatingTime').val().split(':').map((element) => Number(element));
+    if (timeArray) {
+      const [hours, mins] = timeArray;
+      if (hours === 0) {
+        if (mins === 0) {
+          return `${String(hours).padStart(2, '0')}:${String(mins).padStart(2, '0')}`;
+        } else {
+          return `${String(hours).padStart(2, '0')}:${String(mins).padStart(2, '0')} AM`
+        }
+      } else if (hours > 12) {
+        return `${String(hours % 12).padStart(2, '0')}:${String(mins).padStart(2, '0')} PM`;
+      }
+
+      return `${String(hours).padStart(2, '0')}:${String(mins).padStart(2, '0')} AM`
+    }
+  }
+
+  function getCatagory(modalBody) {
+    const catagory = modalBody.find(':selected').text();
+    if (catagory !== 'Select a Catagory') {
+      return catagory
+    } else return null;
+  }
+
+  $('#save-event-data').click(function () {
+    const modalBody = $(this).parent().prev();
+    const title = modalBody.find('#floatingText').val();
+    const date = formatDate(modalBody);
+    const time = formatTime(modalBody);
+    const catagory = getCatagory(modalBody);
+    const location = modalBody.find('#floatingText2').val();
+    const description = modalBody.find('#floatingTextarea2').val();
   });
 });
